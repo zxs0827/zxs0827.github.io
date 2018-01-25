4baaee8c04a1e21d7235511e9f6d3435bcde13cc
zhanghs <zxs0827@qq.com>
2018-01-25 10:38:07 +0800

    @ 20180125 zhanghs | 系统列表

--- screen-show/js/fifth.js
+++ screen-show/js/fifth.js
@@ -2,21 +2,64 @@
 // 1.xtlb 系统列表
 //----------------------------------------------------------------------------------
 
-function xtlbInit (){
-	$.ajax({
-		url: '/path/to/file',
-		type: 'default GET (Other values: POST)',
-		dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
-		data: {param1: 'value1'},
-	})
-	.done(function() {
-		console.log("success");
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
-	});
-	
+function xtlbInit() {
+    var maxLiNumber = 15;
+    var pageShowNumber = 0;
+    var singleHeight = 30;
+
+
+    $.ajax({
+            url: 'https://zxs0827.github.io/screen-show/json/xtlb.json',
+            type: 'GET',
+            dataType: 'json',
+        })
+        .done(function(datajson) {
+            console.log("success");
+            console.log(datajson);
+
+            var getNumber = datajson.data.length ;
+            alert(getNumber);
+
+            if(maxLiNumber >= getNumber) {
+            	$("#xtlb_body").css("height", getNumber*singleHeight);
+            	swiperInit(getNumber);
+            } else {
+            	$("#xtlb_body").css("height", maxLiNumber*singleHeight);
+            	swiperInit(maxLiNumber);
+            }
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
+        });
+
+    // 滚动内容
+    function setContext(data){
+    	$.each(data, function(index, val) {
+    		 /* iterate through array or object */
+    		 $('<div />',{
+    		 	'class' : 'xtlb_li swiper-slide',
+    		 	'html' : '',
+    		 }).appendTo('.swiper-wrapper')
+    	});
+    }
+
+    // 滚动初始化
+    function swiperInit(number) {
+        var swiper = new Swiper('.swiper-container', {
+            direction: 'vertical',
+            loop: true, // 环路
+            slidesPerView: number, //每页显示条数
+            simulateTouch: false, //鼠标拖动事件
+            autoplay: {
+                delay: 1000,
+                disableOnInteraction: false,
+            }
+        });
+    }
+
 }
+
+xtlbInit();
